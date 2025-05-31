8a61a57b2383b9f96f67444a4779ee11153be283
Jacob Hochstetler <jacob.hochstetler@gmail.com>
2025-05-30 22:53:43 -0500

    Refactored and added video download.

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# lltd
-LinkedIn Learning Transcript Downloader
+# LinkedIn Learning Downloader (lld)
+
+## Overview
+`lld` is a tool designed to download course videos and transcripts from LinkedIn Learning. 
+It automates the process of scraping course content, saving videos, and generating transcripts in both `.txt` and `.json` formats.
+I got tired of fighting the LinkedInLearning API, and other Python programs, so I broken down and wrote this with `chromedp`.
+
+## Features
+- **SSO Login**: Supports enterprise Single Sign-On (SSO) for authentication.
+- **Video Download**: Automatically downloads course videos in `.mp4` format.
+- **Transcript Extraction**: Extracts and saves transcripts in `.txt` or `.json` formats.
+- **Course Parsing**: Parses course structure to identify sections and videos.
+
+## Requirements
+- **Go**: Ensure Go is installed on your system.
+- **Chromedp**: Used for browser automation.
+- **LinkedIn Learning Account**: Required to access course content.
+
+## Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/jh125486/lld.git
+   cd lld
+   ```
+
+2. Install dependencies:
+   ```bash
+   go mod tidy
+   ```
+
+## Usage
+1. Build the tool:
+   ```bash
+   go build -o lld
+   ```
+
+2. Run the tool with the course URL:
+   ```bash
+   ./lld -courseURL="https://www.linkedin.com/learning/course-name"
+   ```
+3. Required flags:
+    - `-course`: The URL of the LinkedIn Learning course you want to download.
+    - `-sso`: The URL for enterprise Single Sign-On (SSO).
+
+One of the following flags is also required:
+    - `-transcripts`: Download transcripts.
+    - `-videos`: Download videos.
+
+4. Optional flags:
+    - `-json`: Save transcripts in `.json` format.
+    - `-backoff`: Set a custom backoff time for retries.
+    - `-timeout`: Set a custom timeout for browser operations.
+
+### Example command:
+   ```bash
+   ./lld go run . \
+      -sso 'http://www.linkedin.com/checkpoint/enterprise/login/74650474?application=learning&appInstanceId=46437124&authModeId=6536630950934134784' \
+      -course 'https://www.linkedin.com/learning/how-to-speak-smarter-when-put-on-the-spot/spontaneity-takes-preparation-20294708?u=74650474' \
+      -transcripts \
+      -json \
+      -videos
+   ```
+
+## Notes
+- Ensure you have access to the course content before using the tool.
+- Use responsibly and adhere to LinkedIn Learning's terms of service.
+
+## License
+This project is licensed under the MIT License. See the `LICENSE` file for details.
--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -15,50 +19,68 @@ import (
 )
 
 type VideoEntry struct {
-	Href     string `json:"href"`
-	Section  string `json:"section"`
-	Index    int    `json:"index"`
-	Duration string `json:"duration"`
+	Href       string `json:"href"`
+	Section    string `json:"section"`
+	Title      string `json:"title"`
+	Index      int    `json:"index"`
+	Duration   string `json:"duration"`
+	Transcript string `json:"transcript,omitempty"`
+	filename   string
 }
 
+var invalidRE = regexp.MustCompile(`[^a-zA-Z0-9._-]+`)
+
 func sanitizeFileName(s string) string {
-	s = strings.ReplaceAll(s, " | LinkedIn Learning", "")
+	s = strings.ReplaceAll(s, "| LinkedIn Learning", "")
 	s = strings.TrimSpace(s)
-	re := regexp.MustCompile(`[^a-zA-Z0-9._-]+`)
-	return re.ReplaceAllString(s, "_")
+	return invalidRE.ReplaceAllString(s, "_")
 }
 
 const videoParseJS = `(() => {
 	const sections = Array.from(document.querySelectorAll("section.classroom-toc-section"));
 	const results = [];
 	for (const section of sections) {
-	const title = section.querySelector(".classroom-toc-section__toggle-title")?.innerText.trim();
-	const videos = section.querySelectorAll("li.classroom-toc-item");
-	let index = 0;
-	for (const video of videos) {
-		const link = video.querySelector("a.classroom-toc-item__link");
-		const spans = Array.from(video.querySelectorAll("span"))
-		const duration = spans.map(el => el.innerText.trim())
-		.find(text => text.toLowerCase().endsWith("video")) || ""
-		if (!link) continue;
-		index++;
-		results.push({
-		href: link.href,
-		section: title,
-		index: index,
-		duration: duration.split(' ').slice(0, -1).join('')
-		});
-	}
+		const sectionName = section.querySelector(".classroom-toc-section__toggle-title")?.innerText.trim();
+			const videos = section.querySelectorAll("li.classroom-toc-item");
+		let index = 0;
+		for (const video of videos) {
+			const link = video.querySelector("a.classroom-toc-item__link");
+			const spans = Array.from(video.querySelectorAll("span"));
+			const title = Array.from(video.querySelector('.classroom-toc-item__title').childNodes)
+				.find(n => n.nodeType === Node.TEXT_NODE && n.textContent.trim())
+ 				.textContent.trim();
+			const duration = spans.map(el => el.innerText.trim())
+				.find(text => text.toLowerCase().endsWith("video")) || "";
+			if (!link) continue;
+			index++;
+			results.push({
+				href: link.href,
+				section: sectionName,
+				title: title,
+				index: index,
+				duration: duration.split(' ').slice(0, -1).join('')
+			});
+		}
 	}
 	return results;
 })()`
 
 func main() {
-	courseURL := flag.String("course", "", "URL of the the course to download.")
 	ssoURL := flag.String("sso", "", "URL to the enterprise SSO sign-on.")
+	courseURL := flag.String("course", "", "URL of the the course to download.")
+	dlTranscripts := flag.Bool("transcripts", false, "Whether or not to download transcripts.")
+	saveJSON := flag.Bool("json", false, "Whether or not to output the transcript as JSON.")
+	dlVideos := flag.Bool("videos", false, "Whether or not to download videos.")
+	timeout := flag.Duration("timeout", time.Hour, "Timeout for the entire operation.")
+	backoff := flag.Duration("backoff", time.Minute, "How often to wait between backoff retries.")
 	flag.Parse()
 
-	ctx := newChromeDPCtx()
+	if !*dlVideos && !*dlTranscripts {
+		log.Fatal("❌ You must specify at least one of -transcripts or -videos to download.")
+	}
+
+	ctx, cancel := newChromeDPCtx(*timeout)
+	defer cancel()
 
 	if err := ssoLogin(ctx, *ssoURL); err != nil {
 		log.Fatal(err)
@@ -72,54 +94,121 @@ func main() {
 	log.Printf("🎯 Found %d video(s) across %d sections\n", len(videos), countSections(videos))
 
 	for i, video := range videos {
-		log.Printf("▶️  [%d/%d] %v: %s \n", i+1, len(videos), video.Section, strings.TrimPrefix(video.Href, *courseURL))
-		if err := downloadTranscript(ctx, video); err != nil {
-			log.Printf("%v -> skipping.", err)
+		log.Printf("▶️ [%d/%d] %v: %s \n", i+1, len(videos), video.Section, video.Title)
+		if err := visitVideo(ctx, video.Href, *backoff, 0); err != nil {
+			log.Printf("🙅 failed to visit video: %v", err)
+			continue
+		}
+		if *dlTranscripts {
+			if err := downloadTranscript(ctx, video, *saveJSON); err != nil {
+				log.Printf("%v -> skipping.", err)
+				continue
+			}
+		}
+		if *dlVideos {
+			if err := downloadVideo(ctx, video); err != nil {
+				log.Printf("%v -> skipping.", err)
+				continue
+			}
 		}
 	}
 
-	log.Println("✅ All transcripts saved.")
+	log.Println("✅ All courses info saved.")
 }
 
-func downloadTranscript(ctx context.Context, video VideoEntry) error {
-	u, err := url.Parse(video.Href)
-	if err != nil {
-		return fmt.Errorf("❌ bad url: %w", err)
-	}
-	u.RawQuery = ""
-	video.Href = u.String()
-
-	if err := visitVideo(ctx, video, 0); err != nil {
-		return fmt.Errorf("🙅 failed to visit video: %w", err)
-	}
-
-	var (
-		title string
-		lines []string
-	)
-	if err = chromedp.Run(ctx,
+func downloadTranscript(ctx context.Context, video VideoEntry, saveJSON bool) error {
+	var lines []string
+	if err := chromedp.Run(ctx,
 		chromedp.ScrollIntoView(`button[id*="TRANSCRIPT"]`, chromedp.ByQuery),
 		chromedp.Click(`button[id*="TRANSCRIPT"]`, chromedp.ByQuery),
 		chromedp.Sleep(2*time.Second),
 		chromedp.WaitVisible(`.content-transcript-line`, chromedp.ByQuery),
-		chromedp.Title(&title),
 		chromedp.Evaluate(`Array.from(document.querySelectorAll('.content-transcript-line')).map(x => x.textContent.trim())`, &lines),
 	); err != nil {
 		return fmt.Errorf("⚠️ failed to scrape: %v", err)
 	}
+	video.Transcript = strings.Join(lines, "\n")
 
-	cleanTitle := sanitizeFileName(title)
-	cleanSection := sanitizeFileName(video.Section)
+	ext := "txt"
+	if saveJSON {
+		ext = "json"
+	}
+	filename := video.filename + "." + ext
+	f, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("❌ failed to create file %s: %w", filename, err)
+	}
+	defer func() {
+		_ = f.Close()
+	}()
 
-	filename := fmt.Sprintf("%s.%02d.%s.txt", cleanSection, video.Index, cleanTitle)
-	content := fmt.Sprintln("Title:", strings.TrimSuffix(title, " | LinkedIn Learning"))
-	content += fmt.Sprintln("URL:", video.Href)
-	content += fmt.Sprintln("Duration:", video.Duration)
-	content += fmt.Sprintln("Transcript:\n", strings.Join(lines, "\n"))
+	if saveJSON {
+		if err := json.NewEncoder(f).Encode(video); err != nil {
+			return fmt.Errorf("❌ failed to write JSON: %w", err)
+		}
+		log.Printf("💾 transcript saved: %s\n", filename)
+		return nil
+	}
 
-	os.WriteFile(filename, []byte(content), 0644)
-	log.Printf("💾 saved %s\n", filename)
+	var sb strings.Builder
+	sb.WriteString("URL: " + video.Href + "\n")
+	sb.WriteString("Section: " + video.Section + "\n")
+	sb.WriteString("Title: " + video.Title + "\n")
+	sb.WriteString("Index: " + strconv.Itoa(video.Index) + "\n")
+	sb.WriteString("Duration: " + video.Duration + "\n")
+	sb.WriteString("Transcript:\n" + video.Transcript + "\n")
+	if _, err := f.WriteString(sb.String()); err != nil {
+		return fmt.Errorf("❌ failed to write transcript: %w", err)
+	}
+	log.Printf("💾 transcript saved: %s\n", filename)
 
+	return nil
+}
+
+func downloadVideo(ctx context.Context, video VideoEntry) error {
+	var videoURL string
+	if err := chromedp.Run(ctx,
+		chromedp.WaitVisible(`video.vjs-tech`, chromedp.ByQuery),
+		chromedp.AttributeValue(`video.vjs-tech`, "src", &videoURL, nil),
+	); err != nil {
+		return fmt.Errorf("⚠️ failed to find video: %v", err)
+	}
+	if videoURL == "" {
+		return fmt.Errorf("⚠️ empty video URL found")
+	}
+
+	filename := video.filename + ".mp4"
+	f, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("❌ failed to create file %s: %w", filename, err)
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, videoURL, http.NoBody)
+	if err != nil {
+		return fmt.Errorf("❌ failed to create request: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("❌ failed to download video: %w", err)
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("❌ server returned status: %s", resp.Status)
+	}
+
+	// Copy the response body to the file
+	if _, err = io.Copy(f, resp.Body); err != nil {
+		return fmt.Errorf("❌ failed to save video: %w", err)
+	}
+
+	log.Printf("💾 video saved: %s\n", filename)
 	return nil
 }
 
@@ -129,10 +218,20 @@ func parseCourseVideos(ctx context.Context, courseURL string) ([]VideoEntry, err
 	if err := chromedp.Run(ctx,
 		chromedp.Navigate(courseURL),
 		chromedp.WaitVisible(`section.classroom-toc-section`, chromedp.ByQuery),
-		chromedp.Sleep(1*time.Second),
+		chromedp.Sleep(time.Second),
 		chromedp.Evaluate(videoParseJS, &videos),
 	); err != nil {
 		return nil, err
+	}
+	for i, v := range videos {
+		// Sigh. Sometimes LinkedIn Learning actually has bad URLs in courses.. catch them early here.
+		u, err := url.Parse(v.Href)
+		if err != nil {
+			return nil, fmt.Errorf("❌ bad url: %w", err)
+		}
+		u.RawQuery = "" // Remove any query trash at the end.
+		videos[i].Href = u.String()
+		videos[i].filename = sanitizeFileName(fmt.Sprintf("%s.%02d.%s", v.Section, v.Index, v.Title))
 	}
 
 	return videos, nil
@@ -146,21 +245,23 @@ func ssoLogin(ctx context.Context, u string) error {
 	)
 }
 
-func newChromeDPCtx() context.Context {
+func newChromeDPCtx(to time.Duration) (context.Context, context.CancelFunc) {
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("headless", false),
 		chromedp.Flag("disable-gpu", false),
 		chromedp.Flag("start-maximized", true),
 	)
 
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
-	ctx, cancel := chromedp.NewContext(allocCtx)
-	defer cancel()
-	ctx, cancel = context.WithTimeout(ctx, time.Hour)
-	defer cancel()
+	allocCtx, allocCancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	ctx, chromeCancel := chromedp.NewContext(allocCtx)
+	ctx, timeoutCancel := context.WithTimeout(ctx, to)
 
-	return ctx
+	// Return a combined cancel function that calls all cancel funcs in reverse order.
+	return ctx, func() {
+		timeoutCancel()
+		chromeCancel()
+		allocCancel()
+	}
 }
 
 func countSections(videos []VideoEntry) int {
@@ -171,31 +272,32 @@ func countSections(videos []VideoEntry) int {
 	return len(seen)
 }
 
+// Eh. This is a bit of a hack, but LinkedIn Learning has a tendency to rate limit requests if you hit them too fast.
 const maxRetry = 6
 
-func visitVideo(ctx context.Context, video VideoEntry, count int) error {
+func visitVideo(ctx context.Context, href string, backoff time.Duration, count int) error {
 	var (
 		rateLimited   bool
 		hasTranscript bool
 	)
 	if err := chromedp.Run(ctx,
-		chromedp.Navigate(video.Href),
+		chromedp.Navigate(href),
 		chromedp.Evaluate(`!!document.querySelector('.error-body')`, &rateLimited),
 		chromedp.Evaluate(`!!document.querySelector("button[id*='TRANSCRIPT']")`, &hasTranscript),
 	); err != nil {
-		if count < maxRetry {
-			log.Printf("❌ navigation failed (%v), retrying\n", err)
-			time.Sleep(time.Minute)
-			return visitVideo(ctx, video, count+1)
+		if count >= maxRetry {
+			return fmt.Errorf("❌ navigation failed, stopping: %w", err)
 		}
-		return fmt.Errorf("❌ navigation failed, stopping: %w", err)
+		log.Printf("❌ navigation failed (%v), retrying\n", err)
+		time.Sleep(backoff)
+		return visitVideo(ctx, href, backoff, count+1)
 	}
 	if rateLimited {
 		log.Println("🚧 Rate limited. Sleeping a minute and retrying...")
-		time.Sleep(time.Minute)
-		return visitVideo(ctx, video, count+1)
+		time.Sleep(backoff)
+		return visitVideo(ctx, href, backoff, count+1)
 	} else if !hasTranscript {
-		return fmt.Errorf("⏭️ skipping (no transcript): %s", video.Href)
+		return fmt.Errorf("⏭️ skipping (no transcript): %s", href)
 	}
 
 	return nil
